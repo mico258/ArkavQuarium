338620ed438a1b20e129a80efa79b8eb43338cf0
betheazdavida <betheazia17@gmail.com>
2018-04-22 12:20:01 +0700

    edit snail

--- src/animal/Snail.java
+++ src/animal/Snail.java
@@ -1,10 +1,35 @@
 package animal;
 
 
+import droppable.Koin;
+
 public class Snail extends Animal {
 
+//    private boolean isSearching;
     public Snail(double xPosition, double yPosition, String image) {
         super(xPosition, yPosition, image);
+//        this.isSearching = false;
     }
 
+    void doSomething() {
+
+    }
+
+//    bool getIsSearching(){
+//        return this->isSearching;
+//    }
+
+//    void Snail::setIsSearching(bool a){
+//        this->isSearching = a;
+//    }
+    
+    void moveToCoin(Koin k){
+        double x0 = this.getxPosition();
+
+        if(k.getxPosition()<this.getxPosition()){
+            this.setxPosition(x0-0.1);
+        } else if(k.getxPosition()>this.getxPosition()){
+            this.setxPosition(x0+0.1);
+        }
+    }
 }
